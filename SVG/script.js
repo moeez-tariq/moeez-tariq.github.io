f8af7b172ab0cd0283acffd5fe5f852854ff6523
moeez-tariq <mt4610@nyu.edu>
2023-02-25 21:19:33 +0400

    SVG Tesla Cyber Truck

---
+++ SVG/script.js
@@ -0,0 +1,35 @@
+let carBody = document.getElementById("Car_Body")
+let interiorDetail = document.getElementById("Interior_Color")
+let otherDetail = document.getElementById("Details")
+let background = document.getElementById("Background")
+let textbox = document.getElementById("Text")
+let windows = document.getElementById("Windows")
+let tyres = document.getElementById("Tyres")
+
+carBody.addEventListener("click", () => {
+    alert("The body is an exoskeleton, which is to say, the body panels are welded together to form the structure of the truck. This is not body-on-frame construction like a conventional pickup truck, nor a collection of panels bolted and welded into a unitized body shell like a typical car or crossover.")
+})
+
+interiorDetail.addEventListener("click", () => {
+    alert("The body is an exoskeleton, which is to say, the body panels are welded together to form the structure of the truck. This is not body-on-frame construction like a conventional pickup truck, nor a collection of panels bolted and welded into a unitized body shell like a typical car or crossover.")
+})
+
+otherDetail.addEventListener("click", () => {
+    alert("The body is an exoskeleton, which is to say, the body panels are welded together to form the structure of the truck. This is not body-on-frame construction like a conventional pickup truck, nor a collection of panels bolted and welded into a unitized body shell like a typical car or crossover.")
+})
+
+background.addEventListener("click", () => {
+    alert("The Tesla Cybertruck is an battery electric light-duty truck announced by Tesla, Inc. in 2019. Three models have been announced, with EPA range estimates of 400–800 kilometers (250–500 mi) and an estimated 0–100 km/h (0–62 mph) time of 2.9–6.5 seconds, depending on the model.")
+})
+
+textbox.addEventListener("click", () => {
+    alert("The Tesla Cybertruck is an battery electric light-duty truck announced by Tesla, Inc. in 2019. Three models have been announced, with EPA range estimates of 400–800 kilometers (250–500 mi) and an estimated 0–100 km/h (0–62 mph) time of 2.9–6.5 seconds, depending on the model.")
+})
+
+windows.addEventListener("click", () => {
+    alert("Tesla Armor Glass (aka Durable Glass for Vehicle) is a two-ply laminated glass product: A multilayer glass stack for a vehicle windshield with improved durability. The multilayer glass stack includes an external-facing glass layer, an internal-facing glass layer, and an adhesive interlayer positioned between the external-facing and internal-facing glass layers. The external-facing glass layer comprises borosilicate.")
+})
+
+tyres.addEventListener("click", () => {
+    alert("The first aftermarket wheel designed for the Telsa Cybertruck through a collaboration between Unplugged Performance and accomplished sports car designer Sasha Selipanov, with all the right brutalist neo-futuristic design notes and incredible strength to take you anywhere on Earth or beyond.")
+})
